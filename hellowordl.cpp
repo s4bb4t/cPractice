222059cf24aaebab9b13a0b479b10dc7d0c1de4c
Dmitry Bratishkin <mentalrapewhr@gmail.com>
2024-09-06 19:11:05 +0300

    Сдеалал калькулятор средней сложности

--- hellowordl.cpp
+++ hellowordl.cpp
@@ -1,16 +1,53 @@
 #include <iostream>
-#include <vector>
-#include <string>
 
-using namespace std;
+int main() {
+    while (true) { 
+        int num1, num2, result;
+        char oper;
 
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+        num1 = Req();
+        oper = ReqOperator();
+        num2 = Req();
 
-    for (const string& word : msg)
-    {
-        cout << word << " ";
+        switch (oper) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    std::cout << "Ошибка: деление на ноль!" << std::endl;
+                    continue;
+                }
+                break;
+            default:
+                std::cout << "Ошибка: некорректный оператор!" << std::endl;
+                continue; 
+        }
+        std::cout << "Результат: " << result << std::endl;
     }
-    cout << endl;
+
+    return 0; 
+}
+
+int Req() {
+    int num;
+    std::cout << "Введите число: ";
+    std::cin >> num;
+    return num;
+}
+
+char ReqOperator() {
+    char oper;
+    std::cout << "Введите знак арифметической операции (+, -, *, /): ";
+    std::cin >> oper;
+    return oper;
 }
